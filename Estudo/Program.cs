01715660c820d6f45bd0c4affcaaa21055989c02
Sabrinavf <contactsabrinafernandes@gmail.com>
2023-11-17 17:32:00 -0300

    Adicionar arquivos de projeto.

---
+++ Estudo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estudo
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            double n = 3.14159;
+            double raio, area, N, A;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = n * (Math.Pow(raio,2));
+
+            Console.WriteLine("A=" + area.ToString("F4"),CultureInfo.InvariantCulture);
+
+
+
+
+        } 
+    }
+}
